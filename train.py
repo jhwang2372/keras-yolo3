b44eb37b492309aff17609c4ce11ffe17fe6b37b
jhwang2372 <jhwang2372@gmail.com>
2019-05-06 17:12:27 +0800

    Revert "Train from scratch with No. classes = 2."
    This reverts commit 6bb55f854cd4bf1c4d17dc8823a2c4ca275232a1.

--- train.py
+++ train.py
@@ -15,7 +15,7 @@ from yolo3.utils import get_random_data
 
 def _main():
     annotation_path = 'train.txt'
-    log_dir = 'logs/003/'
+    log_dir = 'logs/002/'
     classes_path = 'model_data/voc_classes.txt'
     anchors_path = 'model_data/orange_anchors.txt'
     class_names = get_classes(classes_path)
@@ -29,9 +29,8 @@ def _main():
         model = create_tiny_model(input_shape, anchors, num_classes,
             freeze_body=2, weights_path='model_data/tiny_yolo_weights.h5')
     else:
-        # model = create_model(input_shape, anchors, num_classes,
-        #     freeze_body=2, weights_path='model_data/trained_weights_stage_1.h5') # make sure you know what you freeze
-        model = create_model(input_shape, anchors, num_classes)
+        model = create_model(input_shape, anchors, num_classes,
+            freeze_body=2, weights_path='model_data/trained_weights_stage_1.h5') # make sure you know what you freeze
 
     logging = TensorBoard(log_dir=log_dir)
     checkpoint = ModelCheckpoint(log_dir + 'ep{epoch:03d}-loss{loss:.3f}-val_loss{val_loss:.3f}.h5',
@@ -71,7 +70,7 @@ def _main():
     if True:
         for i in range(len(model.layers)):
             model.layers[i].trainable = True
-        model.compile(optimizer=Adam(lr=1e-3), loss={'yolo_loss': lambda y_true, y_pred: y_pred}) # recompile to apply the change
+        model.compile(optimizer=Adam(lr=1e-4), loss={'yolo_loss': lambda y_true, y_pred: y_pred}) # recompile to apply the change
         print('Unfreeze all of the layers.')
 
         batch_size = 8 # note that more GPU memory is required after unfreezing the body
@@ -80,7 +79,7 @@ def _main():
             steps_per_epoch=max(1, num_train//batch_size),
             validation_data=data_generator_wrapper(lines[num_train:], batch_size, input_shape, anchors, num_classes),
             validation_steps=max(1, num_val//batch_size),
-            epochs=500,
+            epochs=100,
             initial_epoch=0,
             callbacks=[logging, checkpoint, reduce_lr, early_stopping])
         model.save_weights(log_dir + 'trained_weights_final.h5')
@@ -103,7 +102,7 @@ def get_anchors(anchors_path):
     return np.array(anchors).reshape(-1, 2)
 
 
-def create_model(input_shape, anchors, num_classes, load_pretrained=False, freeze_body=False,
+def create_model(input_shape, anchors, num_classes, load_pretrained=True, freeze_body=2,
             weights_path='model_data/yolo_weights.h5'):
     '''create the training model'''
     K.clear_session() # get a new session
