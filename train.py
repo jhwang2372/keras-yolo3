eb2e1a04f4696c4784a55076e18b4b2d9257f871
jhwang2372 <jhwang2372@gmail.com>
2019-05-20 18:00:37 +0800

    train the network with (416,416) with max_boxes = 100

--- train.py
+++ train.py
@@ -15,7 +15,7 @@ from yolo3.utils import get_random_data
 
 def _main():
     annotation_path = 'train.txt'
-    log_dir = 'logs/005/'
+    log_dir = 'logs/006/'
     classes_path = 'model_data/voc_classes.txt'
     anchors_path = 'model_data/orange_anchors.txt'
     class_names = get_classes(classes_path)
@@ -73,7 +73,7 @@ def _main():
         model.compile(optimizer=Adam(lr=1e-4), loss={'yolo_loss': lambda y_true, y_pred: y_pred}) # recompile to apply the change
         print('Unfreeze all of the layers.')
 
-        batch_size = 1 # note that more GPU memory is required after unfreezing the body
+        batch_size = 8 # note that more GPU memory is required after unfreezing the body
         print('Train on {} samples, val on {} samples, with batch size {}.'.format(num_train, num_val, batch_size))
         model.fit_generator(data_generator_wrapper(lines[:num_train], batch_size, input_shape, anchors, num_classes),
             steps_per_epoch=max(1, num_train//batch_size),
